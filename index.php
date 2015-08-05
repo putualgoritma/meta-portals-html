fe20e0d040c2ff9e3771053bc26b87f7caada5b9
putualgoritma <putualgoritma@gmail.com>
2015-08-05 17:57:21 +0800

    testicum bro

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Untitled Document Bro</title>
 <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
 <link href="css/style.css" type="text/css" rel="stylesheet" />
 <style>
